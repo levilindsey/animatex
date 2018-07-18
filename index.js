f8036427f1113f3b1f5e9c0f0225879c6c2e0e7a
Levi Lindsey <levi@levi.codes>
2018-07-18 05:54:51 +0000

    Remove lsl-gulp-tasts

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = {}; // FIXME: Point this to dist
+export * from './src/animation-job';
+export * from './src/animator';
+export * from './src/frame-latency-profiler';
+export * from './src/persistent-animation-job';
+export * from './src/transient-animation-job';
